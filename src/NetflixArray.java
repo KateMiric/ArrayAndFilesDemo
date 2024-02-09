630cf0616e21bc59fb7415c1eaa0f2e7dd0c86a7
25mirkat <25mirkat@students.dlshcch.co.za>
2024-02-09 13:17:25 +0200

    added clean showName

---
+++ src/NetflixArray.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NetflixArray {
+    private final int ARRAY_MAZ = 20000;
+  NetflixShow[] showArray = new NetflixShow[ARRAY_MAZ];
+
+  private int noOfItems = 0;
+
+  public NetflixArray (String fileName){
+     loadFile(fileName);
+  }
+public void loadFile(String fileName) {
+    try {
+        File netflixFile = new File(fileName);
+        Scanner fileReader = new Scanner(netflixFile);
+        while(fileReader.hasNextLine()) {
+            String currLine = fileReader.nextLine();
+            Scanner parser = new Scanner(currLine);
+            parser.useDelimiter(";");
+            String show = parser.next();
+            int count = Integer.parseInt(parser.next());
+            showArray[noOfItems] = new NetflixShow(show,count);
+            noOfItems ++;
+        }
+    } catch (FileNotFoundException e) {
+        JOptionPane.showMessageDialog(null,"error reading file");
+    }
+}// end load file
+
+    public void saveFile(String fileName){
+      try {
+          File netflixFile = new File(fileName);
+          FileWriter newFile = new FileWriter(netflixFile);
+          for (int index = 0; index < noOfItems; index++) {
+              newFile.write(showArray[index].forFile());
+          }      // end for
+          newFile.flush();
+          newFile.close();
+      } catch (IOException e) {
+          JOptionPane.showMessageDialog(null,"error writing file");
+      }// end catch
+    }
+}
---
+++ src/NetflixShow.java
@@ -0,0 +1,51 @@
+public class NetflixShow {
+    private String showName;
+    private int viewCount;
+
+    public String getShowName() {
+        return showName;
+    }// get show
+
+    public NetflixShow(String showName, int viewCount) {
+        this.showName = showName;
+        this.viewCount = viewCount;
+    }
+
+    public void setShowName(String showName) {
+        this.showName = showName;
+    }// sey show
+
+    public int getViewCount() {
+        return viewCount;
+    }// get count
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }// ser count
+
+    public String toString() {
+        String result = "";
+        result += String.format("%50s%-20d", showName,viewCount);
+        return result;
+    }
+    public String forFile(){
+        String result = "";
+        result += String.format(showName +";"+ viewCount);
+        return result;
+    }
+
+    public void cleanShowName(){
+        while (showName.contains("?")){
+            showName = showName.replace("?","");
+        }
+        while (showName.contains("/")){
+            showName = showName.replace("/","");
+        }
+        while(showName.contains("  ")){
+            showName = showName.replace(" ","");
+        }
+
+    }
+
+
+}
